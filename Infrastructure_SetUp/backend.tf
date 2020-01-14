ab3ab82e16734c8bcf4a9bf79ce5adf04f0931aa
Asthana <ankit.asthana@capgemini.com>
2020-01-14 06:51:58 +0100

    Commit 12#

--- Infrastructure_SetUp/backend.tf
+++ Infrastructure_SetUp/backend.tf
@@ -1,7 +1,8 @@
 data "terraform_remote_state" "wordpress_state" {
   backend = "s3"
-  config={  bucket     = "wordpress-tfstate"
-    key        = "infra.tfstate"
+  config = {  
+    bucket     = "wordpress-tfstate"
+    key        = "terraform.tfstate"
     region     = "eu-central-1"
     #kms_key_id = "${aws_kms_key.s3_encryption_key}"
   }
