da312b15024fa3fd93d3c54c022fb2c231e05c39
Asthana <ankit.asthana@capgemini.com>
2020-01-14 07:07:49 +0100

    Commit 16#

--- Infrastructure_SetUp/provider.tf
+++ Infrastructure_SetUp/provider.tf
@@ -1,8 +1,8 @@
-/*
+
 terraform {
-  required_version = "= 0.12"
+  required_version = "= 0.10"
 }
-*/
+
 
 
 
--- Web_Hosting/provider.tf
+++ Web_Hosting/provider.tf
@@ -15,10 +15,10 @@ data "terraform_remote_state" "wordpress_state_deploy" {
   }
 }
 
-/*
+
 terraform {
-  required_version = ">= 0.12"
+  required_version = "= 0.10"
 }
-*/
+
 
 
