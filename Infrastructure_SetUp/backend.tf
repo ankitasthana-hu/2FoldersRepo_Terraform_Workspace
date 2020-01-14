89c13937d1f49de96a1330f8d2e25dc4b212b12b
Asthana <ankit.asthana@capgemini.com>
2020-01-14 06:06:11 +0100

    Commit 11#

--- Infrastructure_SetUp/backend.tf
+++ Infrastructure_SetUp/backend.tf
@@ -3,11 +3,13 @@ data "terraform_remote_state" "wordpress_state" {
   config={  bucket     = "wordpress-tfstate"
     key        = "infra.tfstate"
     region     = "eu-central-1"
-    kms_key_id = "${aws_kms_key.s3_encryption_key}"
+    #kms_key_id = "${aws_kms_key.s3_encryption_key}"
   }
 }
 
 ### Please note : expected deletion_window_in_days to be in the range (7 - 30)
+/*
 resource "aws_kms_key" "s3_encryption_key" {
   deletion_window_in_days = 8
 }
+*/
--- Web_Hosting/provider.tf
+++ Web_Hosting/provider.tf
@@ -1,8 +1,9 @@
+/*
 resource "aws_kms_key" "s3_encryption_key" {
 
   deletion_window_in_days = 8
 }
-
+*/
 data "terraform_remote_state" "wordpress_state_deploy" {
   backend = "s3"
 
@@ -10,7 +11,7 @@ data "terraform_remote_state" "wordpress_state_deploy" {
     bucket     = "wordpress-tfstate"
     key        = "deploy.tfstate"
     region     = "eu-central-1"
-    kms_key_id = aws_kms_key.s3_encryption_key
+    #kms_key_id = aws_kms_key.s3_encryption_key
   }
 }
 
